897f228da69de0c0d4ac98f4babb0e606d48b3f6
oaker <icaner@qq.com>
2015-05-30 14:58:11 +0800

    copy files from blog repo

---
+++ config.js
@@ -0,0 +1,6 @@
+config = {
+    blog_name: "Oaker's Blog",
+    github_username: "cyio", // github 账号名
+    github_repo: "blog",      // github 建立的 repo 名
+    per_page: 10,             // 首页每次载入文章列表数量
+};
