d7f0b2aaab104cd64020b665e83c481623945528
cyio <ibeceo@gmail.com>
2015-07-02 17:55:14 +0800

    Add markdown-toc.js

---
+++ js/markdown-toc.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $("h2,h3,h4,h5,h6").each(function(i,item){
+        var tag = $(item).get(0).localName;
+        $(item).attr("id","wow"+i);
+        $("#toc").append('<a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br>');
+        $(".newh2").css("margin-left",0);
+        $(".newh3").css("margin-left",20);
+        $(".newh4").css("margin-left",40);
+        $(".newh5").css("margin-left",60);
+        $(".newh6").css("margin-left",80);
+      });
+ });
