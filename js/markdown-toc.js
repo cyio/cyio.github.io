730b3b2d4069e4599210cd3f1eedecbbdc536c81
cyio <ibeceo@gmail.com>
2015-07-02 18:42:25 +0800

    Fixed markdown-toc

--- js/markdown-toc.js
+++ js/markdown-toc.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-      $("h2,h3,h4,h5,h6").each(function(i,item){
+        $("h2,h3,h4,h5,h6").each(function(i,item){
         var tag = $(item).get(0).localName;
         $(item).attr("id","wow"+i);
-        $("#toc").append('<a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br>');
+        $("#wow0").before('<a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br>');
         $(".newh2").css("margin-left",0);
         $(".newh3").css("margin-left",20);
         $(".newh4").css("margin-left",40);
