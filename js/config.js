fa6f2b2746b9a6c12ed8a13d190a61bc8d3a23b1
cyio <icaner@qq.com>
2016-03-03 15:38:49 +0800

    update token

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ config = {
     github_username: "cyio", // github 账号名
     github_repo: "cyio.github.io",      // github 建立的 repo 名
     per_page: 10,             // 首页每次载入文章列表数量
-    access_token: "0a3ffcc46865e590820e"+"675798b4f5fdc0a332ea"
+    access_token: "90a0de745b08e6d8742d"+"6fe44054cf94d7ef2630"
 };
