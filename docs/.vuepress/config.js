adc05fd7c9c4edfb65839925461b3972b4163e74
cyio <icaner@qq.com>
2018-07-28 21:33:16 +0800

    add cname

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,4 +6,12 @@ module.exports = {
     ['link', { rel: 'manifest', href: '/manifest.json' }],
   ],
   serviceWorker: true,
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '前端规范', link: '/frontEnd/' },
+      { text: '开发环境', link: '/development/' },
+      { text: '学习文档', link: '/notes/' },
+    ]
+  },
 }
