6fb9f9aa166adc1938c0618dfee7fec2a8eea7b8
cyio <icaner@qq.com>
2018-07-28 19:46:14 +0800

    enable config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Oaker 的文档',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.ico` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+  ],
+  serviceWorker: true,
+}
