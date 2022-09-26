15134a345ac938bae1d31ad319b94977612976df
cyio <cyio@users.noreply.github.com>
2022-09-26 06:02:42 +0000

    deploy: 8eb83512abfe128501bf6825c778cf5d8c37403d

---
+++ assets/cache-request.html.2d65e996.js
@@ -0,0 +1,1 @@
+const e={key:"v-83e47b1a",path:"/javascript/solutions/cache-request.html",title:"\u7F13\u5B58\u8BF7\u6C42",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1664172054e3,contributors:[{name:"cyio",email:"icaner@qq.com",commits:1}]},filePathRelative:"javascript/solutions/cache-request.md"};export{e as data};
